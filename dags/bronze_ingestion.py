53793d36b53ad8ac50a67365f861c633a03b7520
sogalabhi <abhijithsogal@gmail.com>
2026-07-01 19:33:19 +0530

    Add initial project filesand implement docker compose and bronze layer

---
+++ dags/bronze_ingestion.py
@@ -0,0 +1,55 @@
+import datetime
+
+import boto3
+import os
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+from botocore.exceptions import ClientError
+
+def validate_source_file():
+    csv_path = "/opt/airflow/data/online_retail_II.csv"
+    if not os.path.exists(csv_path):
+        raise FileNotFoundError("Source CSV file not found.")
+    return csv_path
+
+def upload_to_bronze(**context):
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url="http://minio:9000",
+        aws_access_key_id=os.environ.get("MINIO_ROOT_USER"),
+        aws_secret_access_key=os.environ.get("MINIO_ROOT_PASSWORD"),
+        region_name="us-east-1"
+    )
+    ti = context["ti"]
+    execution_date = context["ds"]
+    csv_path = ti.xcom_pull(task_ids="validate_source_file")
+    bronze_key = f"bronze/{execution_date}/online_retail_II.csv"
+    try:
+        s3_client.create_bucket(Bucket="lakehouse")
+    except ClientError as e:
+        error_code = None
+        if hasattr(e, "response"):
+            error_code = e.response.get("Error", {}).get("Code")
+        if error_code != "BucketAlreadyOwnedByYou":
+            raise
+
+    s3_client.upload_file(Filename = csv_path, Bucket="lakehouse", Key=bronze_key)
+with DAG(
+    dag_id="bronze_ingestion",
+    start_date=datetime.datetime(2026, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+    task_1 = PythonOperator(
+        task_id="validate_source_file",
+        python_callable=validate_source_file,
+    )
+
+    task_2 = PythonOperator(
+        task_id="upload_to_bronze",
+        python_callable=upload_to_bronze,
+    )
+
+    task_1 >> task_2
